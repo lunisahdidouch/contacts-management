c84bee2bda2fe7d847b0e304f8ad07323a8c4c3f
Lunis <lunis1@live.nl>
2025-03-29 01:04:32 +0100

    Created functions to read and write from and to a json file. Did not connect it to the main app yet

--- src/main/java/org/example/JsonFileReader.java
+++ src/main/java/org/example/JsonFileReader.java
@@ -1,4 +1,17 @@
 package org.example;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.IOException;
+
 public class JsonFileReader {
+    public static void readContacts() throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(new File("contacts-list.json"));
+        String name = jsonNode.get("name").asText();
+        int age = jsonNode.get("age").asInt();
+        String city = jsonNode.get("phoneNumber").asText();
+        String state = jsonNode.get("emailAddress").asText();
+    }
 }
